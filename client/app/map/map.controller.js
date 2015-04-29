61c1ec940489cccd8a0e21cbfaacd4bdcde701dd
Daniel Reissner <mial@danielreissner.com>
2015-04-29 13:37:03 +0200

    added geolocation to map.controller.js with fallback

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('treasuremapApp')
-  .controller('MapCtrl', function ($scope, $http) {
-    $scope.map = { center: { latitude: 52.460531, longitude: 13.521875 }, zoom: 8 };
-  });
+  	.controller('MapCtrl', function ($scope, $http) {
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+          $scope.map = { center: { latitude: position.coords.latitude, longitude: position.coords.longitude }, zoom: 14 }
+      });
+    }
+
+    $scope.map = { center: { latitude: 52.5075419, longitude: 13.4251364 }, zoom: 14 };
+
+    $scope.locations = [];
+
+    $http.get('/api/locations').success(function(locations) {
+    	$scope.locations = locations;
+      	$scope.marker = {
+	    	id: 0,
+	      	coords: {
+	        	latitude: locations[0].coordinates.lat,
+	        	longitude: locations[0].coordinates.lng
+	      	}
+	    };
+    });
+
+ });
