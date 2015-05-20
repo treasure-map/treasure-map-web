2effecadc7aa520fbda83ae8cf9cf7b7f3a1051e
Kageetai <ma@kageetai.net>
2015-05-20 11:15:00 +0200

    get categories and save category for new location

--- client/app/new/new.controller.js
+++ client/app/new/new.controller.js
@@ -5,6 +5,14 @@ angular.module('treasuremapApp')
     $scope.message = 'Hello';
     $scope.newLocation = {};
 
+    $http.get('/api/categories')
+      .success(function (categories) {
+        $scope.categories = categories;
+      })
+      .error(function (data, status, headers, config) {
+        console.log('Error!');
+      });
+
     $scope.addLocation = function (form) {
       $scope.submitted = true;
 
@@ -13,45 +21,37 @@ angular.module('treasuremapApp')
       }
 
       //if (form.$valid) {
-        $http.post('/api/locations', $scope.newLocation).
-          success(function(data, status, headers, config) {
-            console.log('Success!');
-          }).
-          error(function(data, status, headers, config) {
-            console.log('Error!');
-          });
+      $http.post('/api/locations', $scope.newLocation)
+        .success(function (data, status) {
+          console.log('Success!' + status);
+          console.log(data);
+        })
+        .error(function (data, status) {
+          console.log('Error!' + status);
+        });
       //}
     };
 
-  $scope.rate = 7;
-  $scope.max = 10;
-  $scope.isReadonly = false;
+    $scope.rate = 7;
+    $scope.max = 10;
+    $scope.isReadonly = false;
 
-  $scope.hoveringOver = function(value) {
-    $scope.overStar = value;
-    $scope.percent = 100 * (value / $scope.max);
-  };
+    $scope.hoveringOver = function (value) {
+      $scope.overStar = value;
+      $scope.percent = 100 * (value / $scope.max);
+    };
 
-  $scope.ratingStates = [
-    {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
-    {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
-    {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
-    {stateOn: 'glyphicon-heart'},
-    {stateOff: 'glyphicon-off'}
-  ];
+    $scope.ratingStates = [
+      {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+      {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+      {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+      {stateOn: 'glyphicon-heart'},
+      {stateOff: 'glyphicon-off'}
+    ];
 
-$scope.singleModel = 1;
 
-  $scope.radioModel = 'Middle';
-
-  $scope.checkModel = {
-    1: false,
-    2: false,
-    3: false,
-    4: false
-  };
   });
-  
+
 
 
 
