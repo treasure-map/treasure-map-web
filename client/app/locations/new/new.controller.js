2740b5d66700e8acc064adf6b5c5b79f8cf14de3
Daniel Reissner <mail@danielreissner.com>
2015-06-11 15:28:01 +0200

    s3 upload works!

--- client/app/locations/new/new.controller.js
+++ client/app/locations/new/new.controller.js
@@ -34,115 +34,36 @@ angular.module('treasuremapApp')
           $scope.alerts.push({type: 'success', msg: 'New Location successfully added!'});
         })
         .error(function (data, status) {
-          console.log('Error!' + status);          $scope.alerts.push({type: 'danger', msg: 'Couln\'t add new location!'});
+          console.log('Error!' + status);
+          $scope.alerts.push({type: 'danger', msg: 'Couln\'t add new location!'});
         });
       //}
     };
 
   })
-  .controller('UploadCtrl', function ($scope, Upload) {
-    var getExpiryTime,
-        getTodayShort,
-        getToday,
-        date;
-
-    date = new Date();
-    getExpiryTime = function () {
-      return '' + (date.getFullYear()) + '-' + (date.getMonth() + 1) + '-' +
-        (date.getDate() + 1) + 'T' + (date.getHours() + 3) + ':' + '00:00.000Z';
-    };
-    getToday = function () {
-      return date.toISOString();
-    }
-    getTodayShort = function () {
-      return date.toISOString().substring(0, 10).replace(/-/g,'');
-    }
-
+  .controller('UploadCtrl', function ($scope) {
     var S3_SECRET_KEY = 'OAiEQA8MY7o+kGPhYIB9M4W1tqKQokTun2xZehsg',
         S3_ACCESS_KEY = 'AKIAJTIPCXDWAFSGFM3A',
         S3_BUCKET = ***REMOVED***,
-        S3_POLICY = {
-      'expiration': getExpiryTime(),
-      'conditions': [
-      {'bucket': S3_BUCKET},
-      {'acl': 'private'},
-      ['starts-with', '$Content-Type', 'image/'],
-      ['content-length-range', 0, 5242880],
-      {'x-amz-credential': S3_ACCESS_KEY + '/' + getTodayShort() + '/eu-central-1/s3/aws4_request'},
-      {'x-amz-algorithm': 'AWS4-HMAC-SHA256'},
-      {'x-amz-date': getToday() }
-    ]};
-
-    var s3 = new AWS.S3({accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY, region: ***REMOVED***, signatureVersion: 'v4'});
-
-    var policy_string = JSON.stringify(S3_POLICY);
-    var policy_base64 = window.btoa(policy_string);
-
-    function sign(msg, key) {
-      return CryptoJS.HmacSHA256( key, msg );
-    }
-
-    //var signature = CryptoJS.HmacSHA1(policy_base64, S3_SECRET_KEY);
-    function getSignatureKey(key, dateStamp, regionName, serviceName) {
-
-      var kDate = sign(dateStamp, 'AWS4' + key);
-      var kRegion = sign(regionName, kDate);
-      var kService = sign(serviceName, kRegion);
-      var kSigning = sign('aws4_request', kService);
-
-      return kSigning;
-    }
-    var signature = getSignatureKey(S3_SECRET_KEY, getTodayShort(), ***REMOVED***, 's3');
-    var signature_base64 = window.btoa(signature);
-
-    /*
-    var signatureString = JSON.stringify(S3_POLICY);
-    var timestamp = Math.round(+new Date() / 1000);
-    var message = window.btoa(signatureString);
-
-    var result = CryptoJS.HmacSHA1(message, S3_SECRET);
-    var hexsig = CryptoJS.enc.Hex.stringify(result);
-    var S3_SIGNATURE = message + ' ' + hexsig + ' ' + timestamp;
-    */
-
-    console.log(signature_base64);
+        S3 = new AWS.S3({accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY, region: ***REMOVED***, signatureVersion: 'v4'});
 
     $scope.$watch('files', function () {
       $scope.upload($scope.files);
     });
 
+    $scope.urls = [];
+
     $scope.upload = function (files) {
       if (files && files.length) {
         for (var i = 0; i < files.length; i++) {
           var file = files[i];
-          /*var params = {Bucket: S3_BUCKET, Key: file.name, ContentType: file.type, Body: file};
-            s3.upload(params, function (err, data) {
-              if (err) console.log(err, err.stack);
-              else     console.log(data);
-            });*/
-          Upload.upload({
-            url: 'https://' + S3_BUCKET + '.s3.eu-central-1.amazonaws.com',
-            method: 'POST',
-            transformRequest: function (data, headersGetter) {
-              var headers = headersGetter();
-              delete headers['Authorization'];
-              return data;
-            },
-            fields : {
-              key: file.name,
-              AWSAccessKeyId: S3_ACCESS_KEY,
-              acl: 'private',
-              policy: policy_base64,
-              signature: signature_base64,
-              'Content-Type': file.type !== '' ? file.type : 'application/octet-stream',
-              filename: file.name
-            },
-            file: file
-          }).progress(function (evt) {
-            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-          }).success(function (data, status, headers, config) {
-            console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+          var params = {Bucket: S3_BUCKET, Key: 'images/' + file.name, ContentType: file.type, Body: file};
+          S3.upload(params, function (err, data) {
+            if (err) {
+              console.log(err, err.stack);
+            } else {
+              $scope.urls.push(data.Location);
+            }
           });
         }
       }
