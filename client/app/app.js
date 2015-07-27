065457306e787eaa7f6fa62be98aa78444550971
Kageetai <ma@kageetai.net>
2015-07-27 13:48:28 +0200

    removed angular-animate due to error with boostrap-modal and friends filter on users route

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,6 @@ angular.module('treasuremapApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngAnimate',
   'ui.router',
   'ui.bootstrap',
   'uiGmapgoogle-maps',
--- client/app/users/users.controller.js
+++ client/app/users/users.controller.js
@@ -5,6 +5,7 @@ angular.module('treasuremapApp')
     $scope.currentUser = Auth.getCurrentUser();
     $scope.isFriend = Auth.isFriend;
     $scope.users = User.query();
+    $scope.allUsers = false;
 
     console.log($scope.users);
   });
